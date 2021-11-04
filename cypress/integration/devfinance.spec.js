5d2ac2ede475a1f4977e4e0c83f300ca26d200bf
Thiago Folly <thiago.oliveira@rgsistemas.com.br>
2021-11-04 20:29:18 -0300

    primeiro commit

---
+++ cypress/integration/devfinance.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+// get - mapear elementos usando seletor
+// contains - mapear elemento usando texto
+
+describe("DevFinance", () => {
+  it.only("Adicionar duas novas transações e excluir a segunda", () => {
+    cy.visit("https://devfinance-agilizei.netlify.app/#");
+
+    cy.get("a[onclick*=open]").click();
+    cy.get("#description").type("Freela");
+    cy.get("#amount").type("12");
+    cy.get("#date").type("2021-11-03");
+    cy.contains("button", "Salvar").click();
+
+    cy.get("table tbody tr").should("have.length", 1);
+
+    cy.get("a[onclick*=open]").click();
+    cy.get("#description").type("Freela");
+    cy.get("#amount").type("-20");
+    cy.get("#date").type("2021-11-03");
+    cy.contains("button", "Salvar").click();
+
+    cy.get("table tbody tr").should("have.length", 2);
+
+    cy.get("table tbody tr").eq(1).find("img[onclick*=remove]").click();
+  });
+});
